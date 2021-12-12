2cc98f46f7c70754cbcdb46ee805ab509d979aa3
Mathieu <mathdub78@gmail.com>
2021-12-12 17:00:17 +0100

    initial commit on master

---
+++ cmake-build-debug/CMakeFiles/SubView.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SubView.dir/main.c.o"
+  "SubView"
+  "SubView.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/SubView.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/SubView.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for SubView.
+# This may be replaced when dependencies are built.
