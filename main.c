555792381f9f9f01ef0eedfba55c26d597f55b3a
Mathieu <mathdub78@gmail.com>
2021-12-20 14:22:28 +0100

    adding list,add and fav boutton

--- main.c
+++ main.c
@@ -57,7 +57,7 @@ void drawprimarypage(){
 
     sprintf(realtime, "%d:%d", hour, minute);
 
-    char station[255] = "Nation";
+    char station[255] = "Nation️";
     char Destination[255] = "La Défense";
     char stopButton[255] = "Stop";
 
@@ -71,6 +71,9 @@ void drawprimarypage(){
     ALLEGRO_COLOR primary_blue = al_map_rgb(22,75,156);
     ALLEGRO_COLOR alternative_grey = al_map_rgb(188,188,188);
     ALLEGRO_BITMAP*metrologo;                // l'image
+    ALLEGRO_BITMAP*heart;
+    ALLEGRO_BITMAP*iledefrance;
+    ALLEGRO_BITMAP*RATP;
 
     al_flip_display(); // go editing mode
     al_clear_to_color(al_map_rgb(255,255,255)); // draw blank screen
@@ -103,9 +106,31 @@ void drawprimarypage(){
 
     al_draw_filled_rectangle(50,813, 1256, 947, alternative_grey); // bottom left rectangle
 
-    al_draw_filled_rectangle(100, 830, 400, 930, primary_blue);                                            //
-                                                                                                                                // EXIT BUTTON
-    al_draw_text(Parisine_font_small, al_map_rgb(255,255,255), 140, 815, 0,stopButton);   //
+    al_draw_filled_rounded_rectangle(20,957, 129, 1066, 30, 30, primary_grey); // add rectangle
+    al_draw_filled_rounded_rectangle(35,1007, 114, 1016, 2, 2, primary_blue); // line1
+    al_draw_filled_rounded_rectangle(70,972, 79, 1051, 2, 2,  primary_blue); // line2
+
+
+    al_draw_filled_rounded_rectangle(893,957, 1002, 1066, 30, 30, primary_grey); // list rectangle
+    al_draw_filled_rounded_rectangle(908,979, 987, 988, 2, 2, primary_blue); // line1
+    al_draw_filled_rounded_rectangle(908,1007, 987, 1016, 2, 2,  primary_blue); // line2
+    al_draw_filled_rounded_rectangle(908,1035, 987, 1044, 2, 2,  primary_blue); // line3
+
+
+    al_draw_filled_rounded_rectangle(1777,957, 1886, 1066, 30, 30, primary_grey); // favourite rectangle
+    heart = al_load_bitmap("../Ressources/images/heart.png"); // heart image
+    al_draw_bitmap(heart,1777,957,0);
+
+
+    iledefrance = al_load_bitmap("../Ressources/images/iledefrance.png"); // logo ile de france
+    al_draw_bitmap(iledefrance,1283,849,0);
+
+    RATP = al_load_bitmap("../Ressources/images/RATP.png"); // logo RATP
+    al_draw_bitmap(RATP,1727,820,0);
+
+
+    al_draw_filled_rectangle(100, 830, 400, 930, primary_blue);                                                 // EXIT BUTTON
+    al_draw_text(Parisine_font_small, al_map_rgb(255,255,255), 140, 815, 0,stopButton);            //
 
     al_flip_display(); // end editing mode
 
@@ -177,7 +202,7 @@ int main(int argc, char *argv[])
         printf("Audio clip sample not loaded!\n");
         return -1;
     }
-  // al_play_sample(sample, 1.0, 0.0, 1.0, ALLEGRO_PLAYMODE_ONCE, 0);
+ //  al_play_sample(sample, 1.0, 0.0, 1.0, ALLEGRO_PLAYMODE_ONCE, 0);
 
     //
     // End Audio
@@ -221,10 +246,28 @@ int main(int argc, char *argv[])
 
         al_get_mouse_state(&mouse);
 
-        if(100 < mouse.x && mouse.x < 400 && 830<mouse.y && mouse.y <930 && mouse.buttons &1 ){
+        if(100 < mouse.x && mouse.x < 400 && 830<mouse.y && mouse.y <930 && mouse.buttons &1 ){ // clic on exit button
             printf("stop button pressed");
             fin=1;
         }
+
+        if(20 < mouse.x && mouse.x < 129 && 957<mouse.y && mouse.y <1066 && mouse.buttons &1 ){ // clic on add rectangle ?
+            printf("add button pressed");
+            fin=1;
+        }
+
+        if(893 < mouse.x && mouse.x < 1002 && 957<mouse.y && mouse.y <1066 && mouse.buttons &1 ){ // clic on list rectangle ?
+            printf("list button pressed");
+            fin=1;
+        }
+
+        if(1777 < mouse.x && mouse.x < 1886 && 957<mouse.y && mouse.y <1066 && mouse.buttons &1 ){ // clic on fav rectangle ?
+            printf("fav button pressed");
+            fin=1;
+        }
+
+
+
     };
 
 
